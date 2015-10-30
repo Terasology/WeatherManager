8b575785390bbfcead0f3f5154f50b94007341ee
Rasmus Praestholm <cervator@gmail.com>
2015-10-30 01:23:35 -0400

    Adjust for recent engine change (vector2i tweak).

--- src/main/java/org/terasology/weatherManager/components/WeatherGenerator.java
+++ src/main/java/org/terasology/weatherManager/components/WeatherGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 MovingBlocks
+ * Copyright 2015 MovingBlocks
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,11 +16,8 @@
 package org.terasology.weatherManager.components;
 
 import org.terasology.entitySystem.Component;
-import org.terasology.math.Vector2i;
+import org.terasology.math.geom.Vector2i;
 
-/**
- * Created by Linus on 5-11-2014.
- */
 public class WeatherGenerator implements Component {
     Vector2i position;
 }
